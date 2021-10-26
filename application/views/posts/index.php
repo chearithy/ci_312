f2dd5f45ffa344eb13a8b04e5d6091285324298b
Chea Rithy <crithy@vattanacbank.local>
2021-10-26 17:02:02 +0700

    Word_limiter

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -4,7 +4,7 @@
     <div class="shadow-sm p-3 mb-5 bg-body rounded">
         <h5><?php echo $post['title']; ?><br></h5>
         <small class="text-muted">Post date: <?php echo $post['created_at']; ?></small>
-        <p><?php echo $post['body']; ?></p>
+        <p><?php echo word_limiter($post['body'], 50); ?></p>
         <a class="btn btn-info btn-sm" href="<?php echo site_url('/posts/'.$post['slug']); ?>">Read more</a>
     </div>
 <?php endforeach; ?>
